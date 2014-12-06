5a6aa5fda201fc4fb1b53433db93e1a95e2553b8
Ralf Claussnitzer <ralf.claussnitzer@slub-dresden.de>
2014-12-06 11:53:39 +0100

    Initial commit

---
+++ src/main/java/org/purl/sword/server/fedora/fileHandlers/QucosaMETSFileHandler.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2014 Saxon State and University Library Dresden (SLUB)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.purl.sword.server.fedora.fileHandlers;
+
+import org.purl.sword.base.SWORDEntry;
+import org.purl.sword.base.SWORDException;
+import org.purl.sword.base.ServiceDocument;
+import org.purl.sword.server.fedora.baseExtensions.DepositCollection;
+
+public class QucosaMETSFileHandler extends DefaultFileHandler {
+
+    public QucosaMETSFileHandler() {
+        super("application/vnd.qucosa.mets+xml", "");
+    }
+
+    @Override
+    public SWORDEntry ingestDeposit(DepositCollection depositCollection, ServiceDocument serviceDocument) throws SWORDException {
+        return null;
+    }
+}
