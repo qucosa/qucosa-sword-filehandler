5b429ad736fe72cb18d887535e2dcd41b20d30b9
Ralf Claussnitzer <ralf.claussnitzer@slub-dresden.de>
2014-12-10 16:00:47 +0100

    Refactoring

--- src/main/java/org/purl/sword/server/fedora/fileHandlers/QucosaMETSFileHandler.java
+++ src/main/java/org/purl/sword/server/fedora/fileHandlers/QucosaMETSFileHandler.java
@@ -47,7 +47,7 @@ public class QucosaMETSFileHandler extends DefaultFileHandler {
     public static final Namespace SLUB = Namespace.getNamespace("slub", "http://slub-dresden.de");
     public static final String DS_ID_SLUBINFO = "SLUB-INFO";
     private static final Logger log = Logger.getLogger(METSFileHandler.class);
-    private final Map<String, XPath> queries;
+    private final Map<String, XPathQuery> queries;
     private Document metsDocument;
 
     public QucosaMETSFileHandler() throws JDOMException {
@@ -65,8 +65,8 @@ public class QucosaMETSFileHandler extends DefaultFileHandler {
     protected DublinCore getDublinCore(DepositCollection pDeposit) {
         DublinCore dc = new DublinCore();
         try {
-            subjoin(dc.getTitle(), metsDocument, "primary_title");
-            subjoinAll(dc.getIdentifier(), metsDocument, "identifiers");
+            addIfNotNull(dc.getTitle(), queries.get("primary_title").selectValue(metsDocument));
+            addIfNotNull(dc.getIdentifier(), queries.get("identifiers").selectValues(metsDocument));
         } catch (JDOMException e) {
             log.error(e.getMessage());
         }
@@ -92,19 +92,14 @@ public class QucosaMETSFileHandler extends DefaultFileHandler {
         if (e != null) list.add(e);
     }
 
-    private XPath buildQuery(String xpath) throws JDOMException {
-        XPath xp = XPath.newInstance(xpath);
-        xp.addNamespace(METS);
-        xp.addNamespace(MODS);
-        xp.addNamespace(XLINK);
-        xp.addNamespace(SLUB);
-        return xp;
+    private <E> void addIfNotNull(List<E> list, List<E> es) {
+        if (es != null) list.addAll(es);
     }
 
     private Datastream getSlubInfoDatastream() {
         Datastream result = null;
         try {
-            Element el = (Element) queries.get("slubrights_mdwrap").selectSingleNode(metsDocument);
+            Element el = queries.get("slubrights_mdwrap").selectNode(metsDocument);
             if (el != null) {
                 Element content = (Element) el.getChild("xmlData", METS).getChildren().get(0);
                 Document d = new Document((Element) content.clone());
@@ -117,13 +112,13 @@ public class QucosaMETSFileHandler extends DefaultFileHandler {
         return result;
     }
 
-    private Map<String, XPath> initializeXPathQueries() throws JDOMException {
+    private Map<String, XPathQuery> initializeXPathQueries() throws JDOMException {
         final String MODS_PREFIX = "/mets:mets/mets:dmdSec/mets:mdWrap[@MDTYPE='MODS']/mets:xmlData/mods:mods";
         final String METS_AMDSEC_PREFIX = "/mets:mets/mets:amdSec/mets:rightsMD";
-        return new HashMap<String, XPath>() {{
-            put("primary_title", buildQuery(MODS_PREFIX + "/mods:titleInfo[@usage='primary']/mods:title"));
-            put("identifiers", buildQuery(MODS_PREFIX + "/mods:identifier"));
-            put("slubrights_mdwrap", buildQuery(METS_AMDSEC_PREFIX + "/mets:mdWrap[@MDTYPE='OTHER' and @OTHERMDTYPE='SLUBRIGHTS']"));
+        return new HashMap<String, XPathQuery>() {{
+            put("primary_title", new XPathQuery(MODS_PREFIX + "/mods:titleInfo[@usage='primary']/mods:title"));
+            put("identifiers", new XPathQuery(MODS_PREFIX + "/mods:identifier"));
+            put("slubrights_mdwrap", new XPathQuery(METS_AMDSEC_PREFIX + "/mets:mdWrap[@MDTYPE='OTHER' and @OTHERMDTYPE='SLUBRIGHTS']"));
         }};
     }
 
@@ -142,15 +137,44 @@ public class QucosaMETSFileHandler extends DefaultFileHandler {
         }
     }
 
-    private void subjoin(List<String> strings, Document metsDocument, String key) throws JDOMException {
-        Element el = (Element) queries.get(key).selectSingleNode(metsDocument);
-        if (el != null) strings.add(el.getTextTrim());
-    }
+    private class XPathQuery {
+        private final XPath xpath;
 
-    private void subjoinAll(List<String> strings, Document metsDocument, String key) throws JDOMException {
-        for (Object o : queries.get(key).selectNodes(metsDocument)) {
-            Element el = (Element) o;
-            strings.add(el.getTextTrim());
+        public XPathQuery(String xp) throws JDOMException {
+            xpath = XPath.newInstance(xp);
+            xpath.addNamespace(METS);
+            xpath.addNamespace(MODS);
+            xpath.addNamespace(XLINK);
+            xpath.addNamespace(SLUB);
         }
+
+        public String selectValue(Document doc) throws JDOMException {
+            Element el = selectNode(doc);
+            if (el != null) {
+                return el.getTextTrim();
+            } else {
+                return null;
+            }
+        }
+
+        public List<String> selectValues(Document doc) throws JDOMException {
+            final List<Element> els = selectNodes(doc);
+            return new LinkedList<String>() {{
+                for (Element e : els) add(e.getTextTrim());
+            }};
+        }
+
+        public Element selectNode(Document doc) throws JDOMException {
+            return (Element) xpath.selectSingleNode(doc);
+        }
+
+        public List<Element> selectNodes(Document doc) throws JDOMException {
+            LinkedList<Element> resultList = new LinkedList<>();
+            for (Object o : xpath.selectNodes(doc)) {
+                resultList.add((Element) o);
+            }
+            return resultList;
+        }
+
     }
 }
